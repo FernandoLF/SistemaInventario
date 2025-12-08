deeef59ced0888e125edb3e192bb5a63064a7ace
FernandoLF <FernandoLopez29@gmail.com>
2025-12-08 12:58:20 -0600

    feat(inventario): agregar entidad Bodega con configuración inicial y schema Inventario

---
+++ SistemaInventario.AccesoDatos/Migrations/20251208184713_AgregarBodegaMigracion.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarBodegaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Inventario");
+
+            migrationBuilder.CreateTable(
+                name: "Bodega",
+                schema: "Inventario",
+                columns: table => new
+                {
+                    IdBodega = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "NVARCHAR(60)", maxLength: 60, nullable: false),
+                    Descripcion = table.Column<string>(type: "NVARCHAR(100)", maxLength: 100, nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bodega", x => x.IdBodega);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bodega",
+                schema: "Inventario");
+        }
+    }
+}
